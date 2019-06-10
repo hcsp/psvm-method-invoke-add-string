828e9c281e0fa91b92696c6e86209ee5ae3b11e6
zzxx9426 <97177440@qq.com>
2019-06-10 20:05:17 +0800

    addString

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,9 +1,15 @@
 package com.github.hcsp;
 
+import static com.sun.javafx.fxml.expression.Expression.add;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("aaa+bbb=" + add("aaa", "bbb"));
         System.out.println("cat+dog=" + add("cat", "dog"));
+    }
+
+    private static String add(String a, String b){
+        return(a + " " +b);
     }
     // Create an add(String a, String b) method to concatenate two strings
     // 在这里编写一个add(String a, String b)方法进行字符串连接
