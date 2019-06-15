65efc0c61df920d6fbc8e03be1c7a8874bde2f7b
Bo Zhang <bo@gradle.com>
2019-06-15 19:40:58 +0800

    Update repo

--- src/test/java/com/github/hcsp/MainTest.java
+++ src/test/java/com/github/hcsp/MainTest.java
@@ -1,14 +1,15 @@
 package com.github.hcsp;
 
 import com.github.blindpirate.extensions.CaptureSystemOutput;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
     @Test
     @CaptureSystemOutput
     public void importStringUtilsCorrectly(CaptureSystemOutput.OutputCapture capture) {
-        capture.expect(containsStringIgnoringCase("aaa+bbb=aaabbb"));
-        capture.expect(containsStringIgnoringCase("cat+dog=catdog"));
+        capture.expect(Matchers.containsStringIgnoringCase("aaa+bbb=aaabbb"));
+        capture.expect(Matchers.containsStringIgnoringCase("cat+dog=catdog"));
         Main.main(null);
     }
 
