9fb7acd24ff95edbaf43c332d95f12cfdca868a1
astak16 <1500846601@qq.com>
2019-12-15 13:03:19 +0800

    添加一个 add 方法 (#124)
    添加一个 add 方法 (#124)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -5,7 +5,11 @@ public class Main {
         System.out.println("aaa+bbb=" + add("aaa", "bbb"));
         System.out.println("cat+dog=" + add("cat", "dog"));
     }
+
     // Create an add(String a, String b) method to concatenate two strings
     // 在这里编写一个add(String a, String b)方法进行字符串连接
+    public static String add(String str1, String str2) {
+        return str1 + str2;
+    }
 
 }
