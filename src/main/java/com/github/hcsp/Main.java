3553eb1d04a3728c29c07a123225d253b7585a28
7952126 <15521608275@163.com>
2020-03-10 15:31:15 +0800

    字符串相加

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,9 +1,14 @@
 package com.github.hcsp;
 
+import com.sun.org.apache.xpath.internal.objects.XString;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("aaa+bbb=" + add("aaa", "bbb"));
         System.out.println("cat+dog=" + add("cat", "dog"));
+    }
+    public static String add(String a , String b){
+        return "a + b" ;
     }
     // Create an add(String a, String b) method to concatenate two strings
     // 在这里编写一个add(String a, String b)方法进行字符串连接
