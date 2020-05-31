7ef3a1e083f01448adde23a462fea16dc413c45a
linweikun <343206347@qq.com>
2020-05-31 22:29:42 +0800

    modify variable

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -7,7 +7,7 @@ public class Main {
     }
     // Create an add(String a, String b) method to concatenate two strings
     // 在这里编写一个add(String a, String b)方法进行字符串连接
-    public static String add(String left, String right) {
-        return left + right;
+    public static String add(String a, String b) {
+        return a + b;
     }
 }
